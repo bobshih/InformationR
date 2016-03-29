fd813bd718d65f9853d826a7b317c4187d64e8a5
git-apple-school <t101820307@ntut.org.tw>
2016-03-29 15:54:33 +0800

    Get_warc_fields

--- homework_indexer_parser/homework_indexer_parser/ParserFolder/WARCHeaderParser.cs
+++ homework_indexer_parser/homework_indexer_parser/ParserFolder/WARCHeaderParser.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+///
+///Contributor : 101820307
+///
 
 namespace homework_indexer_parser.Parser
 {
@@ -31,6 +34,7 @@ namespace homework_indexer_parser.Parser
     {
         public double WARC_Version;
         public WARCType WARC_Type;
+        public Dictionary<string, string> named_field;
     }
 
     internal class WARCHeaderParser
@@ -39,6 +43,7 @@ namespace homework_indexer_parser.Parser
         {
             WARCHeader header = new WARCHeader();
             GetVersion(reader, ref header);
+            Get_warc_fields(reader, ref header);
         }
 
         private static void GetVersion(StreamReader reader, ref WARCHeader header)
@@ -46,12 +51,52 @@ namespace homework_indexer_parser.Parser
             try
             {
                 string str = reader.ReadLine();
-                header.WARC_Version = double.Parse(str);
+                if (str.Substring(0, 5) != @"WARC/")
+                    throw new ParserException();
+                header.WARC_Version = double.Parse(str.Substring(5));
             }
             catch
             {
-                throw new ParserException("Not Warc File");
+                throw new ParserException("Not Valid Warc File");
             }
+        }
+
+        private static void Get_warc_fields(StreamReader reader, ref WARCHeader header)
+        {
+            try
+            {
+                while (Get_named_field(reader, ref header))
+                    /*DoNothing*/
+                    ;
+                if (!CheckMandatoryFieldName(header))
+                    throw new ParserException();
+            }
+            catch
+            {
+                throw new ParserException("Not Valid Warc Fields");
+            }
+        }
+
+        private static bool CheckMandatoryFieldName(WARCHeader header)
+        {
+            string[] mandatory = { "WARC-Record-ID", "Content-Length", "WARC-Date", "WARC-Type" };
+            foreach (var s in mandatory)
+                if (!header.named_field.ContainsKey(s))
+                    return false;
+            return true;
+        }
+
+        private static bool Get_named_field(StreamReader reader, ref WARCHeader header)
+        {
+            string str = reader.ReadLine();
+            if (str.Length == 0)
+                return false;
+            int index = str.IndexOf(':');
+            if (index == -1)//Not Found
+                throw new ParserException();
+            string field_name = str.Substring(0, index);
+            string field_value = str.Substring(index + 2);
+            header.named_field.Add(field_name, field_value);
         }
     }
 }
