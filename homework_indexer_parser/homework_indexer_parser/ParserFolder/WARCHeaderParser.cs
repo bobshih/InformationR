bd3fa94618eec536b35427d536c8009d9011ae6a
git-apple-school <t101820307@ntut.org.tw>
2016-03-29 17:12:46 +0800

    SomeAPI

--- homework_indexer_parser/homework_indexer_parser/ParserFolder/WARCHeaderParser.cs
+++ homework_indexer_parser/homework_indexer_parser/ParserFolder/WARCHeaderParser.cs
@@ -18,7 +18,7 @@ namespace homework_indexer_parser.Parser
         }
     }
 
-    enum WARCType
+    public enum WARCType
     {
         warcinfo,
         response,
@@ -30,20 +30,27 @@ namespace homework_indexer_parser.Parser
         continuation
     };
 
-    struct WARCHeader
+    public struct WARCHeader
     {
         public double WARC_Version;
         public WARCType WARC_Type;
+        public int content_length;
+        public string ID;
         public Dictionary<string, string> named_field;
+        public char[] content;
     }
 
-    internal class WARCHeaderParser
+    public class WARCHeaderParser
     {
-        public void ReadHeader(StreamReader reader)
+        public static WARCHeader ReadHeader(StreamReader reader)
         {
             WARCHeader header = new WARCHeader();
             GetVersion(reader, ref header);
             Get_warc_fields(reader, ref header);
+            char[] buffer = new char[header.content_length];
+            reader.ReadBlock(buffer,0,header.content_length);
+            header.content = buffer;
+            return header;
         }
 
         private static void GetVersion(StreamReader reader, ref WARCHeader header)
@@ -57,7 +64,7 @@ namespace homework_indexer_parser.Parser
             }
             catch
             {
-                throw new ParserException("Not Valid Warc File");
+                throw new ParserException("Not Warc File");
             }
         }
 
@@ -70,11 +77,25 @@ namespace homework_indexer_parser.Parser
                     ;
                 if (!CheckMandatoryFieldName(header))
                     throw new ParserException();
+                ParseMandatoryField(ref header);
             }
             catch
             {
                 throw new ParserException("Not Valid Warc Fields");
             }
+        }
+
+        private static void ParseMandatoryField(ref WARCHeader header)
+        {
+            string type;
+            header.named_field.TryGetValue("WARC-Type", out type);
+            header.WARC_Type = (WARCType)Enum.Parse(typeof(WARCType), type);
+
+            string length;
+            header.named_field.TryGetValue("Content-Length", out length);
+            header.content_length = int.Parse(length);
+
+            header.named_field.TryGetValue("WARC-Record-ID", out header.ID);
         }
 
         private static bool CheckMandatoryFieldName(WARCHeader header)
---
+++ homework_indexer_parser/homework_indexer_parser/SomethingWARCParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_indexer_parser.Parser
+{
+    internal class HTMLResponse
+    {
+        string id;
+        string content_type;
+        string content;
+    }
+    internal class SomethingWARCParser
+    {
+        FileStream fstream;
+        StreamReader sreader;
+        public SomethingWARCParser(string path)
+        {
+            fstream = File.OpenRead(path);
+            sreader = new StreamReader(fstream);
+        }
+
+        public HTMLResponse ReadNextHtml()
+        {
+            HTMLResponse response = null;
+            while (!sreader.EndOfStream)
+            {
+                var header = WARCHeaderParser.ReadHeader(sreader);
+                if (header.WARC_Type != WARCType.response)
+                    continue;
+
+            }
+            return response;
+        }
+
+    }
+}
