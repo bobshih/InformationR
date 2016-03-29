977db7aed1d622bed694a6ac9adf1269eaa5a6ff
git-apple-school <t101820307@ntut.org.tw>
2016-03-29 18:08:54 +0800

    Sign

--- homework_indexer_parser/homework_indexer_parser/ParserFolder/SomethingWARCParser.cs
+++ homework_indexer_parser/homework_indexer_parser/ParserFolder/SomethingWARCParser.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+///
+///Contributor : 101820307
+///
 
 namespace homework_indexer_parser.Parser
 {
--- homework_indexer_parser/homework_indexer_parser/ParserFolder/WARCStructures.cs
+++ homework_indexer_parser/homework_indexer_parser/ParserFolder/WARCStructures.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+///
+///Contributor : 101820307
+///
 
 namespace homework_indexer_parser.Parser
 {
