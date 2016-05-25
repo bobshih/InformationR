be2d8495ec9a69f9cfe038cafa1cec3c7d6eb3d1
git-apple-school <t101820307@ntut.org.tw>
2016-05-25 13:34:13 +0800

    DEV
    generic test base

--- homework_knn/KNN/TestKNN/KNNTest.cs
+++ homework_knn/KNN/TestKNN/KNNTest.cs
@@ -5,8 +5,135 @@ using System.Collections.Generic;
 
 namespace TestKNN
 {
-    [TestClass]
-    public class KNNTest
+    public abstract class KNNTestBase<CType, DType>
+    {
+        public abstract DType Sample1
+        {
+            get;
+        }
+        public abstract DType Sample2
+        {
+            get;
+        }
+        public abstract DType SampleCloseTo1
+        {
+            get;
+        }
+        public abstract DType SampleEquallyCloseTo1And2
+        {
+            get;
+        }
+        public abstract IComparer<CType> CategoryComparer
+        {
+            get;
+        }
+    }
+    
+    //[TestClass]
+    public abstract class KNNTestBase<CType,DType,DDType>
+    { 
+        protected abstract Func<DType, DType, DDType> DistanceFunction{
+        get;}
+                protected abstract CType Class1
+        {
+            get;
+        }
+
+        protected abstract CType Class2
+        {
+            get;
+        }
+
+        protected abstract DType Sample1
+        {
+            get;
+        }
+
+        protected abstract DType Sample2
+        {
+            get;
+        }
+
+        protected abstract DType SampleCloseTo1
+        {
+            get;
+        }
+
+        protected abstract DType SampleEquallyCloseTo1And2
+        {
+            get;
+        }
+
+        protected abstract KNN<CType,DType> KnnKernal{get;}
+
+        [TestMethod]
+        public void TestSameQueryAndSample()
+        {
+            KnnKernal.AddTrainingData(Sample1, Class1);
+            KnnKernal.AddTrainingData(Sample2, Class2);
+            Assert.AreEqual(1, KnnKernal.FindCategory(Sample1, 1, DistanceFunction));
+            Assert.AreEqual(2, KnnKernal.FindCategory(Sample2, 1, DistanceFunction));
+        }
+
+        [TestMethod]
+        public void TestDifferentQueryAndSample()
+        {
+            KnnKernal.AddTrainingData(Sample1, 1);
+            KnnKernal.AddTrainingData(Sample2, 2);
+            Assert.AreEqual(1, KnnKernal.FindCategory(SampleCloseTo1, 1, DistanceFunction));
+        }
+
+        [TestMethod]
+        public void TestQueryKMoreThanSample()
+        {
+            KnnKernal.AddTrainingData(Sample1, 1);
+            KnnKernal.AddTrainingData(Sample2, 2);
+            Assert.AreEqual(1, KnnKernal.FindCategory(Sample1, 10, DistanceFunction));
+            Assert.AreEqual(2, KnnKernal.FindCategory(Sample2, 10, DistanceFunction));
+        }
+
+        [TestMethod]
+        public void TestAddToSameClass()
+        {
+            KnnKernal.AddTrainingData(Sample1, 1);
+            KnnKernal.AddTrainingData(Sample2, 1);
+            KnnKernal.AddTrainingData(Sample2, 2);
+            Assert.AreEqual(1, KnnKernal.FindCategory(SampleEquallyCloseTo1And2, 2, DistanceFunction));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestQueryFromZeroCategory()
+        {
+            KnnKernal.FindCategory(Sample1, 1, DistanceFunction);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestNegtiveK()
+        {
+            KnnKernal.AddTrainingData(Sample1, 1);
+            KnnKernal.FindCategory(Sample1, -1, DistanceFunction);
+        }
+
+        [TestMethod]
+        public void TestQueryFromZeroCategoryAndNegtiveK()
+        {
+            try
+            {
+                KnnKernal.FindCategory(Sample1, -1, DistanceFunction);
+                Assert.Fail("Should Have Some Exception");
+            }
+            catch (ArgumentException)
+            {
+            }
+            catch (InvalidOperationException)
+            {
+            }
+        }
+
+    //[TestClass]
+    public class KNNTestBase
     {
         protected Func<List<double>, List<double>, double> distanceFunction = DistanceFunction.EuclideanDistanceSquare;
         private List<double> SampleList1
@@ -116,7 +243,7 @@ namespace TestKNN
         }
 
         [TestClass]
-        public class Testtttt : KNNTest
+        public class Testtttt : KNNTestBase
         {
         }
     }
