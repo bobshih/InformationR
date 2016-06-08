6ffec32572c18db1f63e3803ffc33990ea40fa02
Alan Lin <linsanity810917@yahoo.com.tw>
2016-06-08 20:00:13 +0800

    修改average.js的參數

--- FinalProject/js/average.js
+++ FinalProject/js/average.js
@@ -1,8 +1,8 @@
 /*變成8*8後計算畫素的灰度平均值*/
-function Average(vimgData){
+function Average(data){
         var count = 0;
-        for(var i = 0; i < vimgData.length; i++){
-            count += vimgData[i];
+        for(var i = 0; i < data.length; i++){
+            count += data[i];
         }
-        return Math.round(count / vimgData.length);
+        return Math.round(count / data.length);
 },
